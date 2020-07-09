33e77fe36f7352dd7f05add4dbcbaaa5c5fca162
Carolina Carvalho <carvalhosouza97@gmail.com>
2020-07-09 14:32:00 +0200

    Updates

---
+++ precache-manifest.fb8f8338906e4a158172019cc5c17579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "963470fa116d7aab1662eb8633115eb3",
+    "url": "/wizative-project/index.html"
+  },
+  {
+    "revision": "e8e92cc42e860a6c845c",
+    "url": "/wizative-project/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7964e03d8c1524bad832",
+    "url": "/wizative-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e8e92cc42e860a6c845c",
+    "url": "/wizative-project/static/js/2.a583a9ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/wizative-project/static/js/2.a583a9ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7964e03d8c1524bad832",
+    "url": "/wizative-project/static/js/main.17918004.chunk.js"
+  },
+  {
+    "revision": "661c7f823b49adc0c6c5",
+    "url": "/wizative-project/static/js/runtime-main.32881701.js"
+  }
+]);
